ed88f7a3627c8cbc47899b3295d93fcf2bc3d571
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-09-20 08:03:56 -0700

    home info

--- src/content/pages/Home.jsx
+++ src/content/pages/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Home(props) {
     return (
-        <div>
-            Home
+        <div> 
+            Holindary was created with the purpose of planning and saving your holiday experiences. 
+            You may currently signup/login to see the holidays recognized nationally 
+            the US, as well as international holidays. You may save these holidays to 
+            your profile, add what you'd like to do for each holiday, or create your own custom experience.
+            To learn the site's mechanics, see the information below. 
         </div>
     )
 }
